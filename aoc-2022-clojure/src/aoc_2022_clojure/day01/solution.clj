ec20bc04c96342be70b60feb2f18866389351646
rittelle <33598633+rittelle@users.noreply.github.com>
2022-12-02 04:12:59 +0100

    day 01 clojure refactor

--- aoc-2022-clojure/src/aoc_2022_clojure/day01/solution.clj
+++ aoc-2022-clojure/src/aoc_2022_clojure/day01/solution.clj
@@ -6,25 +6,23 @@
 ;; Part 1
 
 (defn read-input
-  "Splits an input string into a sequence where each element contains an index and a vector
-  corresponding to an elf."
+  "Splits an input string into a sequence where each element contains a vector
+  corresponding to the weights that an elf is carrying."
   [input-str]
-  (let [read-elf-calories (fn [s] (map #(Integer/parseUnsignedInt %) (string/split s #"\n")))
-        read-elf (fn [idx s] (vector (+ idx 1) (read-elf-calories s)))
-        read-elves (fn [s] (map-indexed read-elf (string/split s #"\n\n")))]
+  (let [read-elf (fn [s] (map #(Integer/parseUnsignedInt %) (string/split s #"\n")))
+        read-elves (fn [s] (map read-elf (string/split s #"\n\n")))]
     (read-elves input-str)))
 
-(defn get-elf-calories
-  "Return the number of calories carried by a given elf."
-  [elf] (reduce + (second elf)))
-
-(defn find-elf-with-max-calories
-  "Finds and returns the elf with the maximum calorie amount."
-  [data] (reduce #(max-key get-elf-calories %1 %2) data))
+(defn calculate-all-elf-calories
+  "Return the number of calories carried by a list of elves."
+  [elves] (map (partial reduce +) elves))
 
 (defn solution-1
   "Solve the first part with the given input."
-  [input-str] (get-elf-calories (find-elf-with-max-calories (read-input input-str))))
+  [input-str] (->> input-str
+                   (read-input)
+                   (calculate-all-elf-calories)
+                   (reduce max)))
 
 (defn print-solution-1
   "Print a message containing the solution to the first part to stdout."
@@ -32,37 +30,26 @@
 
 ;; Part 2
 
-(defn remove-elf
-  "Remove elf with idx from the collection."
-  [elf-idx collection]
-  (remove #(= elf-idx (first %)) collection))
-
-;; The more efficient, but longer solution
 (defn get-top-n-elves
-  "Find the n elves that carry the most calories."
-  [elves n]
-  (second (reduce (fn [[remaining-elves top] _]
-                    (let [elf (find-elf-with-max-calories remaining-elves)
-                          elf-idx (first elf)
-                          without-elf (remove-elf elf-idx remaining-elves)]
-                      [without-elf (conj top elf)]))
-                  [elves []]
-                  (range n))))
-
-;; The shorter, but less efficient version
-(defn get-top-n-elves
-  "Find the n elves that carry the most calories."
-  [elves n]
-  (let [sorted-elves (reverse (sort-by get-elf-calories elves))]
-    (take n sorted-elves)))
+  "Find the n calories counts from the elves that carry the most calories."
+  [n elves] (->> elves
+                 (sort)
+                 (reverse)
+                 (take n)))
 
 (defn solution-2
   "Solve the second part with the given input."
-  [input-str] (reduce + (map get-elf-calories (get-top-n-elves (read-input input-str) 3))))
+  [input-str] (->> input-str
+                   (read-input)
+                   (calculate-all-elf-calories)
+                   (get-top-n-elves 3)
+                   (reduce +)))
 
 (defn print-solution-2
   "Print a message containing the solution to the second part to stdout."
   [input-str] (println (str "  Part B: Three elves carry " (solution-2 input-str) " calories together.")))
+
+;; Print both parts
 
 (defn print-solution
   "Print a message containing the solution to the complete exercise to stdout."
