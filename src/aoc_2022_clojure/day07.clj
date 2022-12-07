6967fc30db93caba091d5ee6c11ce7ce74af6df1
rittelle <33598633+rittelle@users.noreply.github.com>
2022-12-07 23:50:44 +0100

    day 07 refactor

--- src/aoc_2022_clojure/day07.clj
+++ src/aoc_2022_clojure/day07.clj
@@ -27,8 +27,8 @@
     (read-path new-path-str)                          ; the path is absolute
     (simplify-path (concat current-path (read-path new-path-str)))))
 
-(defn read-ls-output
-  "Return a sequence of pairs [size path] from an ls output."
+(defn read-ls-output-line
+  "Return a sequence of pairs [size path] from a ls output line."
   [current-path output-line]
   (let [[size file] (string/split output-line #" ")
         is-dir (= size "dir")
@@ -37,6 +37,11 @@
     (if is-dir
       ["dir" full-path]
       [int-size full-path])))
+
+(defn read-ls-output
+  "Return a sequence of pairs [size path] from a ls output."
+  [current-path output]
+  (map (partial read-ls-output-line current-path) output))
 
 (defn convert-file-list-to-map
   "Converts a list of files and directories into a tree structure based on maps."
@@ -58,22 +63,7 @@
              (add-entry-to-tree (get tree
                                      first-part
                                      {})
-                                [size rest-part])))))
-
-(defn calculate-total-size
-  "Calculate the total size of a tree or an entry."
-  [tree]
-  (if (number? tree)
-    tree
-    (transduce (map calculate-total-size) + (vals tree))))
-
-(defn flatmap-dirs
-  "Apply f to all directories and return a flattened sequence."
-  [f tree]
-  (if (number? tree)
-    []
-    (concat [(f tree)]
-            (flatten (map (partial flatmap-dirs f) (vals tree))))))
+                                [size rest-part]))) ) )
 
 (defn read-input-1
   "Read the input and split it into a list of pairs."
@@ -88,18 +78,40 @@
                         command (second words)]
                     (case command
                       "ls" (let [[ls-output remaining-session] (split-with #(not (string/starts-with? % "$ ")) remaining-lines)
-                                 new-files (map (partial read-ls-output current-path) ls-output)]
-                             (recur remaining-session current-path (reduce add-entry-to-tree tree new-files)))
-                      "cd" (recur remaining-lines (read-path-in-context current-path (nth words 2)) tree)
-                      (recur remaining-lines current-path tree))))))
+                                 new-files (read-ls-output current-path ls-output)]
+                             (recur remaining-session
+                                    current-path
+                                    (reduce add-entry-to-tree tree new-files)))
+                      "cd" (recur remaining-lines
+                                  (read-path-in-context current-path (nth words 2))
+                                  tree)
+                      (assert false "Unknown command"))))))
+
+(defn calculate-total-size
+  "Calculate the total size of a tree or an entry."
+  [tree]
+  (if (number? tree)
+    tree
+    (transduce (map calculate-total-size) + (vals tree))))
+
+(defn flatmap-dirs
+  "Return a transduces that applies f to all directories and return a flattened sequence."
+  [f]
+  (fn  [rf]
+    (fn ([] (rf))
+      ([result] (rf result))
+      ([result input] (if (number? (val input))
+                        result
+                        (reduce rf (rf result (f (val input)))
+                                (transduce  (flatmap-dirs f) conj [] (val input))))))))
 
 (defn solution-1
   "Solve the first part"
-  [input-str] (->> input-str
-                   (read-input-1)
-                   (flatmap-dirs calculate-total-size)
-                   (filter (partial > 100000))
-                   (reduce +)))
+  [input-str] (transduce
+               (comp (flatmap-dirs calculate-total-size)
+                     (filter (partial > 100000)))
+               +
+               (read-input-1 input-str)))
 
 (defn print-solution-1
   "Print the solution to part 1 to stdout."
@@ -113,11 +125,10 @@
                     required-space 30000000
                     tree (read-input-1 input-str)
                     free-space (- total-space (calculate-total-size tree))
-                    needed-space (- required-space free-space)
-                    sizes (flatmap-dirs calculate-total-size tree)
-                    ]
+                    needed-space (- required-space free-space)]
                 ;; there will be no bigger size than total-space
-                (transduce (filter (partial < needed-space)) min total-space sizes)))
+                (transduce (comp (flatmap-dirs calculate-total-size)
+                                 (filter (partial < needed-space))) min total-space tree)))
 
 (defn print-solution-2
   "Print the solution to part 2 to stdout."
