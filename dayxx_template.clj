9fb9feb301c18c6bea3dae14fdf72f558ef10189
rittelle <33598633+rittelle@users.noreply.github.com>
2022-12-09 11:37:56 +0100

    Update the templates

--- dayxx_template.clj
+++ dayxx_template.clj
@@ -2,18 +2,18 @@
 
 ;; Part 1
 
-(defn read-input-1
+(defn read-input
   "Read the input and split it into a list of pairs."
-  [input-str] 42)
+  [input-str] (to-array-2d (map (partial map #(Character/digit % 10)) (string/split-lines input-str))))
 
 ;; …
 
 (defn solution-1
   "Solve the first part"
   [input-str] (->> input-str
-                   (read-input-1)
-                   ;; …
-                   (reduce +)))
+                   (read-input)
+                   ;; ...
+                   ((constantly 42))))
 
 (defn print-solution-1
   "Print the solution to part 1 to stdout."
@@ -21,18 +21,14 @@
 
 ;; Part 2
 
-(defn read-input-2
-  "Read the input and split it into a list of lists representing the groups."
-  [input-str] 42)
-
 ;; …
 
 (defn solution-2
   "Solve the second part"
   [input-str] (->> input-str
-                   (read-input-2)
-                   ;; …
-                   (reduce +)))
+                   (read-input)
+                   ;; ...
+                   ((constantly 42))))
 
 (defn print-solution-2
   "Print the solution to part 2 to stdout."
@@ -40,9 +36,11 @@
 
 ;; Both parts combined
 
+(def input (slurp (io/resource "day0x-input.txt")))
+
 (defn print-solution
   "Print the solution both parts to stdout."
-  [input-str]
+  []
   (println "Day 0x:")
-  (print-solution-1 input-str)
-  (print-solution-2 input-str))
+  (print-solution-1 input)
+  (print-solution-2 input))
--- dayxx_test_template.clj
+++ dayxx_test_template.clj
@@ -6,8 +6,12 @@
 
 (t/deftest part-1
   (t/testing "Part 1 test input"
+    (t/is (= 42 (sut/solution-1 test-input))))
+  (t/testing "Part 1 actual input"
     (t/is (= 42 (sut/solution-1 test-input)))))
 
 (t/deftest part-2
   (t/testing "Part 2 test input"
+    (t/is (= 42 (sut/solution-2 test-input))))
+  (t/testing "Part 2 actual input"
     (t/is (= 42 (sut/solution-2 test-input)))))
