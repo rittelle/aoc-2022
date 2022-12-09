e592cd21f2468d06fa00c7ec871f24810cf50e5d
rittelle <33598633+rittelle@users.noreply.github.com>
2022-12-09 17:51:57 +0100

    Fix the templates

--- dayxx_test_template.clj
+++ dayxx_test_template.clj
@@ -8,10 +8,10 @@
   (t/testing "Part 1 test input"
     (t/is (= 42 (sut/solution-1 test-input))))
   (t/testing "Part 1 actual input"
-    (t/is (= 42 (sut/solution-1 test-input)))))
+    (t/is (= 42 (sut/solution-1 sut/input)))))
 
 (t/deftest part-2
   (t/testing "Part 2 test input"
     (t/is (= 42 (sut/solution-2 test-input))))
   (t/testing "Part 2 actual input"
-    (t/is (= 42 (sut/solution-2 test-input)))))
+    (t/is (= 42 (sut/solution-2 sut/input)))))
